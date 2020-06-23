a2418fdafbaa4edeee6de10a9cf1af7e9860f8af
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-06-23 13:34:36 +0200

    Add contact form verification

--- script.js
+++ script.js
@@ -8,6 +8,14 @@ const goRightBtn = document.getElementById('btn-go-right');
 
 const container = document.querySelectorAll('.container');
 
+const contactForm = document.getElementById('contact-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const textInput = document.getElementById('text');
+const nameError = document.getElementById('name-error');
+const emailError = document.getElementById('email-error');
+const textError = document.getElementById('text-error');
+
 // Actual section number
 const sectionsNumber = container.length;
 let activeSection = 0;
@@ -38,6 +46,37 @@ const updateDOM = () => {
 };
 
 updateDOM();
+
+// Contact form verification
+const formVerification = (e) => {
+    if (nameInput.value === "") {
+        e.preventDefault();
+        nameError.classList.add('show');
+    } else {
+        nameError.classList.remove('show');
+    }
+
+    // E-mail verification expression
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (emailInput.value === "") {
+        e.preventDefault();
+        emailError.classList.add('show');
+    } else if (!re.test(String(emailInput.value).toLowerCase())) {
+        e.preventDefault();
+        emailError.textContent = 'E-mail address is invalid';
+        emailError.classList.add('show');
+    } else {
+        emailError.classList.remove('show');
+    }
+
+    if (textInput.value === "") {
+        e.preventDefault();
+        textError.classList.add('show');
+    } else {
+        textError.classList.remove('show');
+    }
+}
 
 // Event listeners
 goRightBtn.addEventListener('click', () => {
@@ -75,3 +114,7 @@ contactBtn.addEventListener('click', () => {
     activeSection = 3;
     updateDOM();
 });
+
+contactForm.addEventListener('submit', (e) => {
+    formVerification(e);
+})
