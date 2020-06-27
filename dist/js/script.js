04a82aad4ec662fa3cf7bfb11c9e713e0c4ff89b
Bartek Mogielnicki <b.mogielnicki@gmail.com>
2020-06-27 18:32:33 +0200

    Fix menu button disable

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,88 +1,96 @@
-const init = () => {
-
     const menuBtn = document.getElementById('menu-btn');
     const menu = document.getElementById('menu-nav-container');
     const logo = document.getElementById('logo');
 
-    const contactForm = document.getElementById('contact-form');
-    const nameInput = document.getElementById('name');
-    const emailInput = document.getElementById('email');
-    const textInput = document.getElementById('text');
-    const nameError = document.getElementById('name-error');
-    const emailError = document.getElementById('email-error');
-    const textError = document.getElementById('text-error');
+    const init = () => {
 
-    const titleText = document.getElementById('title-text');
+        const contactForm = document.getElementById('contact-form');
+        const nameInput = document.getElementById('name');
+        const emailInput = document.getElementById('email');
+        const textInput = document.getElementById('text');
+        const nameError = document.getElementById('name-error');
+        const emailError = document.getElementById('email-error');
+        const textError = document.getElementById('text-error');
 
-    const swup = new Swup();
+        const titleText = document.getElementById('title-text');
 
-    // Typewrite text
-    const texts = ["former mechanical constructor...", "future web developer."];
-    let count = 0;
-    let index = 0;
-    let currentText = "";
-    let newText = "";
+        const swup = new Swup();
 
-    if (titleText) {
-        type();
-    }
+        // Typewrite text
+        const texts = ["former mechanical constructor...", "future web developer."];
+        let count = 0;
+        let index = 0;
+        let currentText = "";
+        let newText = "";
 
-    async function type() {
-        if (count === texts.length) {
-            count = 0;
+        if (titleText) {
+            type();
         }
-        currentText = texts[count];
-        newText = currentText.slice(0, ++index);
 
-        titleText.textContent = newText;
+        async function type() {
+            if (count === texts.length) {
+                count = 0;
+            }
+            currentText = texts[count];
+            newText = currentText.slice(0, ++index);
 
-        if (newText.length === currentText.length) {
+            titleText.textContent = newText;
 
-            const sleep = (ms) => {
-                return new Promise(resolve => setTimeout(resolve, ms))
+            if (newText.length === currentText.length) {
+
+                const sleep = (ms) => {
+                    return new Promise(resolve => setTimeout(resolve, ms))
+                };
+
+                await sleep(1500);
+                count++;
+                index = 0;
+            };
+            setTimeout(type, 70);
+        };
+
+        // Contact form verification
+        const formVerification = (e) => {
+            // Name input verification
+            if (nameInput.value === "") {
+                e.preventDefault();
+                nameError.classList.add('show');
+            } else {
+                nameError.classList.remove('show');
             };
 
-            await sleep(1500);
-            count++;
-            index = 0;
+            // E-mail input verification
+            const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+            if (emailInput.value === "") {
+                e.preventDefault();
+                emailError.classList.add('show');
+            } else if (!re.test(String(emailInput.value).toLowerCase())) {
+                e.preventDefault();
+                emailError.textContent = 'E-mail address is invalid';
+                emailError.classList.add('show');
+            } else {
+                emailError.classList.remove('show');
+            };
+
+            // Text input verification
+            if (textInput.value === "") {
+                e.preventDefault();
+                textError.classList.add('show');
+            } else {
+                textError.classList.remove('show');
+            };
         };
-        setTimeout(type, 70);
+
+        // Submit contact form
+        if (contactForm) {
+            contactForm.addEventListener('submit', (e) => {
+                formVerification(e);
+            });
+        };
+
     };
 
-    // Contact form verification
-    const formVerification = (e) => {
-        // Name input verification
-        if (nameInput.value === "") {
-            e.preventDefault();
-            nameError.classList.add('show');
-        } else {
-            nameError.classList.remove('show');
-        };
-
-        // E-mail input verification
-        const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-        if (emailInput.value === "") {
-            e.preventDefault();
-            emailError.classList.add('show');
-        } else if (!re.test(String(emailInput.value).toLowerCase())) {
-            e.preventDefault();
-            emailError.textContent = 'E-mail address is invalid';
-            emailError.classList.add('show');
-        } else {
-            emailError.classList.remove('show');
-        };
-
-        // Text input verification
-        if (textInput.value === "") {
-            e.preventDefault();
-            textError.classList.add('show');
-        } else {
-            textError.classList.remove('show');
-        };
-    };
-
-    // Event listeners
     // Show/hide menu
     menuBtn.addEventListener('click', () => {
         menu.classList.toggle('show');
@@ -90,16 +98,7 @@ const init = () => {
         logo.classList.toggle('move');
     });
 
-    // Submit contact form
-    if (contactForm) {
-        contactForm.addEventListener('submit', (e) => {
-            formVerification(e);
-        });
-    };
+    init();
 
-};
-
-init();
-
-// Reload script after content swap
-document.addEventListener('swup:contentReplaced', init);
+    // Reload script after content swap
+    document.addEventListener('swup:contentReplaced', init);
